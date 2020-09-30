44a70456853c63fb75ef6ca45ce3929cb20324b4
FerMod <tudelaigarcia@gmail.com>
2020-09-30 14:39:36 +0200

    Extract drawer menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'menu/drawer_menu.dart';
+
 void main() => runApp(QuizApp());
 
 class QuizApp extends StatelessWidget {
@@ -17,63 +19,20 @@ class QuizApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   HomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
   @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Drawer Demo'),
+        title: Text(title),
       ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            Container(
-              child: DrawerHeader(
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                ),
-                child: Text(
-                  'Drawer Header',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 24,
-                  ),
-                ),
-              ),
-              padding: EdgeInsets.zero,
-              margin: EdgeInsets.zero,
-            ),
-            ListTile(
-              leading: Icon(Icons.live_help),
-              title: Text('Quiz'),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: Icon(Icons.storage),
-              title: Text('Questions'),
-              onTap: () {},
-            ),
-          ],
-        ),
+      drawer: DrawerMenu(),
+      body: Center(
+        child: Text('Homepage'),
       ),
     );
   }
---
+++ lib/menu/drawer_menu.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_quiz_app/quiz/pages/view_questions.dart';
+import '../quiz/pages/test_route01.dart';
+
+class DrawerMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          Container(
+            child: DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Drawer Header',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            padding: EdgeInsets.zero,
+            margin: EdgeInsets.zero,
+          ),
+          ListTile(
+            leading: Icon(Icons.live_help),
+            title: Text('Quiz'),
+            onTap: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => FirstRoute()),
+              );
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.storage),
+            title: Text('Questions'),
+            onTap: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => QuestionList()),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
