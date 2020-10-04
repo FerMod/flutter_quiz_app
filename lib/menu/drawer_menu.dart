c92902f838171b4ea50f0ff5e1766d409763082e
FerMod <tudelaigarcia@gmail.com>
2020-10-04 16:04:00 +0200

    Change route to the new data table

--- lib/menu/drawer_menu.dart
+++ lib/menu/drawer_menu.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_quiz_app/quiz/pages/questions_data_table.dart';
+import 'package:flutter_quiz_app/quiz/pages/view_questions.dart';
 import '../quiz/pages/test_route01.dart';
 
 class DrawerMenu extends StatelessWidget {
@@ -41,7 +42,8 @@ class DrawerMenu extends StatelessWidget {
             onTap: () {
               Navigator.pushReplacement(
                 context,
-                MaterialPageRoute(builder: (context) => QuestionsDataTable()),
+                //MaterialPageRoute(builder: (context) => QuestionsDataTable()),
+                MaterialPageRoute(builder: (context) => QuestionList()),
               );
             },
           ),
