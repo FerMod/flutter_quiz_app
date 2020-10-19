44d29a03a1c434444494b128139407480ccd3bd3
FerMod <tudelaigarcia@gmail.com>
2020-10-19 13:55:33 +0200

    Add localizations

---
+++ lib/app_localizations.dart
@@ -0,0 +1,47 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import 'l10n/messages_all.dart';
+
+class AppLocalizations {
+  AppLocalizations(this.localeName);
+
+  static Future<AppLocalizations> load(Locale locale) {
+    final name = locale.countryCode.isEmpty ? locale.languageCode : locale.toString();
+    final localeName = Intl.canonicalizedLocale(name);
+
+    return initializeMessages(localeName).then((_) {
+      return AppLocalizations(localeName);
+    });
+  }
+
+  static AppLocalizations of(BuildContext context) {
+    return Localizations.of<AppLocalizations>(context, AppLocalizations);
+  }
+
+  final String localeName;
+
+  String get title {
+    return Intl.message(
+      'Hello World',
+      name: 'title',
+      desc: 'Title for the Demo application',
+      locale: localeName,
+    );
+  }
+}
+
+class AppLocalizationsDelegate extends LocalizationsDelegate<AppLocalizations> {
+  const AppLocalizationsDelegate();
+
+  @override
+  bool isSupported(Locale locale) => ['en', 'es'].contains(locale.languageCode);
+
+  @override
+  Future<AppLocalizations> load(Locale locale) => AppLocalizations.load(locale);
+
+  @override
+  bool shouldReload(AppLocalizationsDelegate old) => false;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
+import 'app_localizations.dart';
 import 'menu/drawer_menu.dart';
 
 void main() => runApp(QuizApp());
@@ -9,6 +11,16 @@ class QuizApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      onGenerateTitle: (context) => AppLocalizations.of(context).title,
+      localizationsDelegates: [
+        const AppLocalizationsDelegate(),
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', ''),
+        const Locale('es', ''),
+      ],
       title: 'Flutter Demo',
       theme: ThemeData(
         brightness: Brightness.dark,
